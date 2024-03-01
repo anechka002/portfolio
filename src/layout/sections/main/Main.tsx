84f55c16edef4707d51ebfd1cb55c2b6d07eff8a
Anna <blinova.anneta@gmail.com>
2024-03-01 17:01:03 +0100

    created the main section

---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components'
+import photo from './../../../assets/images/photo.jpeg'
+import { FlexWrapper } from '../../../components/FlexWrapper'
+
+function Main() {
+  return (
+    <StyledMain>
+      <FlexWrapper align={'center'} justify={'space-around'}>
+        <div>
+          <span>Hi There</span>
+          <Name>I am Svetlana Dyablo</Name>
+          <MainTitle>A Web Developer.</MainTitle>
+        </div>
+        <Photo src={photo} alt="" />
+      </FlexWrapper>
+    </StyledMain>
+  )
+}
+
+export default Main
+
+const StyledMain = styled.div`
+  min-height: 100vh;
+  background-color: #e4dbdb;
+`
+
+const MainTitle = styled.h1``
+
+const Name = styled.h2``
+
+const Photo = styled.img`
+  width: 350px;
+  height: 430px;
+  object-fit: cover;
+`
