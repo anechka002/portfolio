89dbf4770c73fb5c95534c01128121593c71afe6
Anna <blinova.anneta@gmail.com>
2024-03-01 17:04:48 +0100

    add components to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 import Header from './layout/header/Header';
+import Main from './layout/sections/main/Main';
+import Skills from './layout/sections/skills/Skills';
+import Works from './layout/sections/works/Works';
+import Testimony from './layout/sections/testimony/Testimony';
+import Contact from './layout/sections/contacts/Contacts';
 
 function App() {
   return (
     <div>
-      <Header />
-
+      <Header/>
+      <Main/>
+      <Skills/>
+      <Works/>
+      <Testimony/>
+      <Contact/>
     </div>
   );
 }
