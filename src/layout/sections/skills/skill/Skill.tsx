6e7f2fdbd937c4e5e92390f94e56c0148c58fb8c
Anna <blinova.anneta@gmail.com>
2024-03-15 11:13:10 +0100

    add Mixin

--- src/layout/sections/skills/skill/Skill.tsx
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Icon from '../../../../components/icon/Icon';
 import { FlexWrapper } from '../../../../components/FlexWrapper';
+import { theme } from '../../../../styles/Theme';
 
 type SkillPropsType = {
   iconId: string
@@ -26,9 +27,13 @@ function Skill(props: SkillPropsType) {
 export default Skill;
 
 const StyledSkill = styled.div`
-  width: 380px;
+  width: 330px;
+  flex-grow: 1;
   padding: 65px 20px 40px;
   
+  @media ${theme.media.mobile} {
+    padding: 62px 0 40px;
+  }
 `
 
 export const IconWrapper = styled.div`
