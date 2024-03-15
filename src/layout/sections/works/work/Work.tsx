314d8a2659a07f82ebb77baaaec96bb26d1c5833
Anna <blinova.anneta@gmail.com>
2024-03-15 11:16:07 +0100

    update Work component

--- src/layout/sections/works/work/Work.tsx
+++ src/layout/sections/works/work/Work.tsx
@@ -31,8 +31,8 @@ export default Work
 
 const StyledWork = styled.div`
   background-color: ${theme.colors.secondaryBg};
-  max-width: 540px;
-  width: 100%;
+  width: 330px;
+  flex-grow: 1;
 
   ${TabLink} {
     padding: 10px 0;
@@ -41,28 +41,14 @@ const StyledWork = styled.div`
       margin-left: 20px;
     }
   }
+
+  @media ${theme.media.desktop} {
+    max-width: 540px;
+  }
 `
 
 const ImgWrapper = styled.div`
   position: relative;
-
-  &:hover {
-    &::before {
-      content: '';
-      position: absolute;
-      left: 0;
-      right: 0;
-      top: 0;
-      bottom: 0;
-      backdrop-filter: blur(4px);
-      background: rgba(0, 0, 0, 0.3);
-    }
-
-    ${Button} {
-      opacity: 1;
-    }
-    
-  }
 
   ${Button} {
     opacity: 0;
@@ -74,6 +60,38 @@ const ImgWrapper = styled.div`
     &::before {
       width: 100%;
       height: 100%;
+    }
+  }
+
+  &::before {
+      content: '';
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      backdrop-filter: blur(4px);
+      background: rgba(0, 0, 0, 0.3);
+      opacity: 0;
+    }
+
+  &:hover {
+    &::before {
+      opacity: 1;
+    }
+
+    ${Button} {
+      opacity: 1;
+    }
+  }
+
+  @media ${theme.media.tablet} {
+    &::before {
+      opacity: 1;
+    }
+
+    ${Button} {
+      opacity: 1;
     }
   }
 `
