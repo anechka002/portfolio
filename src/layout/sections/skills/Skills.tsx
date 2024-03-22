a286acdb009fcdc135cfc9dd6ddfeec7a75a8328
Anna <blinova.anneta@gmail.com>
2024-03-22 15:08:50 +0100

    connect react-awesome-reveal library

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -3,7 +3,8 @@ import { FlexWrapper } from '../../../components/FlexWrapper'
 import { SectionTitle } from '../../../components/SectionTitle'
 import Skill from './skill/Skill'
 import { Container } from '../../../components/Container'
-import {S} from './Skills_Styles'
+import {S} from './Skills_Styles';
+import { Fade } from "react-awesome-reveal";
 
 const skillData = [
   {
@@ -45,12 +46,14 @@ const Skills: React.FC = () => {
         <SectionTitle>My Skills</SectionTitle>
         <FlexWrapper wrap={'wrap'} justify={'space-between'}>
 
-          {skillData.map((s, index) => {
-            return <Skill key={index}
-                          iconId={s.iconId} 
-                          title={s.title} 
-                          description={s.description}/>
-          })}
+          <Fade cascade={true} damping={0.2}>
+            {skillData.map((s, index) => {
+              return <Skill key={index}
+                            iconId={s.iconId} 
+                            title={s.title} 
+                            description={s.description}/>
+            })}
+          </Fade>
   
         </FlexWrapper>
       </Container>
